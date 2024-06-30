31f21fb0633697226ff9d5de1b38dc483ac3aaa4
Ktn.beb <krittanon.lak11@gmail.com>
2024-06-30 20:24:56 +0700

    about page, rout

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import NavBar from "../../components/Navbar/NavBar";
+import { motion } from "framer-motion";
+import AboutUs from "../../components/About/AboutUs";
+import Footer from "../../components/Footer/Footer";
+
+function About() {
+    return (
+        <>
+            <div className="h-[100vh] flex flex-col">
+                <NavBar />
+                <motion.hr
+                    className="border-slate-300"
+                    initial={{ width: 0, opacity: 0 }}
+                    animate={{ width: "100%", opacity: 1 }}
+                    transition={{ duration: 2 }}
+                />
+                <AboutUs />
+                <Footer />
+            </div>
+        </>
+    );
+}
+
+export default About;
