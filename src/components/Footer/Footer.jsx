a39141484e57e11a4065f9492bd640db5f16ba5f
Ktn.beb <krittanon.lak11@gmail.com>
2024-06-30 18:49:08 +0700

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="p-6 text-center bg-[#011313] text-white">
+            <p>copyright 2024 ; bemabae</p>
+        </div>
+    );
+}
+
+export default Footer;
