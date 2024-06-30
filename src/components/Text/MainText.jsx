0781270859ce64bde00b9abbe51e47b29861f00b
Ktn.beb <krittanon.lak11@gmail.com>
2024-06-30 20:24:33 +0700

    edit attribute class

--- src/components/Text/MainText.jsx
+++ src/components/Text/MainText.jsx
@@ -5,10 +5,10 @@ function MainText() {
     return (
         <>
             <div className="inline-block md:flex-1">
-                <header class="text-center mt-12">
-                    <h1 class="text-5xl font-bold text-slate-600">Welcome to my shop!</h1>
+                <header className="text-center mt-12">
+                    <h1 className="text-5xl font-bold text-slate-600">Welcome to my shop!</h1>
                 </header>
-                <section class="mt-7 p-10">
+                <section className="mt-7 p-10">
                     <article>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aliquid, reprehenderit, itaque
