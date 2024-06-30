b50a9c9642275527866994ca2ff8c3a507681a2e
Ktn.beb <krittanon.lak11@gmail.com>
2024-06-30 18:59:03 +0700

    add loaing Newproduct while fetch data

--- src/components/Product/NewProduct.jsx
+++ src/components/Product/NewProduct.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { category } from "../../utils/category";
 import NewProductCard from "../Card/NewProductCard/NewProdcutCard";
 import axios from "axios";
+import ReactLoading from "react-loading";
 
 function NewProduct() {
     const [newProduct, setNewProduct] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         getNewProduct(category);
     }, []);
 
-    console.log(newProduct);
-
     const getNewProduct = async (category) => {
         try {
+            setLoading(true);
             const results = [];
             for (const name of category) {
                 const response = await axios.get(
@@ -22,6 +22,7 @@ function NewProduct() {
                 );
                 results.push(response.data);
             }
+            setLoading(false);
             setNewProduct(results);
         } catch (e) {
             console.log("err", e);
@@ -32,16 +33,28 @@ function NewProduct() {
         <div>
             <h1 className="text-5xl font-bold text-center">New Product</h1>
 
-            <div className="flex gap-10 flex-wrap justify-center mt-10">
-                {newProduct.map((arr) =>
-                    arr.map((item) => (
-                        <NewProductCard
-                            item={item}
-                            key={item.id}
-                        />
-                    ))
-                )}
-            </div>
+            {loading && (
+                <div className="flex justify-center h-[550px] items-center">
+                    <ReactLoading
+                        type="cubes"
+                        color="bg-gradient-to-r from-slate-300 to-gray-800"
+                        width={150}
+                    />
+                </div>
+            )}
+
+            {!loading && (
+                <div className="flex gap-10 flex-wrap justify-center mt-10">
+                    {newProduct.map((arr) =>
+                        arr.map((item) => (
+                            <NewProductCard
+                                item={item}
+                                key={item.id}
+                            />
+                        ))
+                    )}
+                </div>
+            )}
         </div>
     );
 }
