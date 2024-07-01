beadb2e8015f4012c5ca076d3ec2d90685d6ca29
Ktn.beb <krittanon.lak11@gmail.com>
2024-07-02 00:20:35 +0700

    create MainText (reuseable) component

---
+++ src/components/Text/MainTextProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainTextProduct({ title }) {
+    return (
+        <header className="text-center mt-10">
+            <h1 className="text-5xl font-bold capitalize">{title}</h1>
+        </header>
+    );
+}
+
+export default MainTextProduct;
