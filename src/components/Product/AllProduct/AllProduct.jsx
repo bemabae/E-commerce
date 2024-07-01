b4333a2b4efb3887dc7f52e007937f500d118762
Ktn.beb <krittanon.lak11@gmail.com>
2024-07-02 00:22:52 +0700

    create Allproduct

---
+++ src/components/Product/AllProduct/AllProduct.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useProductStore } from "../../../store/store";
+import AllProductsCard from "../../Card/AllProductsCard/AllProductsCard";
+import ReactLoading from "react-loading";
+
+function AllProduct() {
+    const allProduct = useProductStore((state) => state.allProduct);
+
+    return (
+        <div className="flex-1 py-10 flex flex-wrap gap-10 justify-center">
+            {allProduct.length == 0 && (
+                <div className="flex justify-center h-[550px] items-center">
+                    <ReactLoading
+                        type="cubes"
+                        color="bg-gradient-to-r from-slate-300 to-gray-800"
+                        width={150}
+                    />
+                </div>
+            )}
+            {allProduct.map((item) => (
+                <AllProductsCard
+                    item={item}
+                    key={item.id}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default AllProduct;
