fa3e6588c9bc21e77146ff74dd32305f35477179
Ktn.beb <krittanon.lak11@gmail.com>
2024-07-02 00:19:26 +0700

    Add New path

--- src/components/Card/NewProductCard/NewProdcutCard.jsx
+++ src/components/Card/NewProductCard/NewProdcutCard.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { Rating, RatingStar } from "flowbite-react";
 
 function NewProductCard({ item }) {
     return (
         <motion.div
-            className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shado flex flex-col justify-between"
+            className="w-full max-w-sm bg-white border border-gray-200 rounded-lg flex flex-col justify-between"
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.3 }}
         >
-            <a
-                href="#"
+            <Link
+                to={`/product/${item.id}/detail`}
                 className="flex-1 flex justify-center"
             >
                 <img
@@ -19,75 +21,33 @@ function NewProductCard({ item }) {
                     width={300}
                     height={300}
                 />
-            </a>
+            </Link>
             <div className="px-5 pb-5">
-                <a href="#">
+                <Link to={`/product/${item.id}/detail`}>
+                    <h4># {item.id}</h4>
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900">{item.title}</h5>
-                </a>
+                </Link>
                 <div className="flex items-center mt-2.5 mb-5">
-                    <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                        <svg
-                            className="w-4 h-4 text-yellow-300"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 22 20"
+                    <Rating className="w-[100%] flex justify-center lg:justify-start mt-3 lg:mt-0">
+                        <RatingStar />
+                        <p className="ml-2 text-sm font-bold text-gray-900">{item.rating?.rate}</p>
+                        <span className="mx-1.5 h-1 w-1 rounded-full bg-gray-500 " />
+                        <a
+                            href="#"
+                            className="text-sm font-medium text-gray-900 underline hover:no-underline "
                         >
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg
-                            className="w-4 h-4 text-yellow-300"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 22 20"
-                        >
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg
-                            className="w-4 h-4 text-yellow-300"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 22 20"
-                        >
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg
-                            className="w-4 h-4 text-yellow-300"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 22 20"
-                        >
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg
-                            className="w-4 h-4 text-gray-200 "
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 22 20"
-                        >
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                    </div>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ">
-                        {item.rating.rate}
-                    </span>
-                    &nbsp;
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ">
-                        {item.rating.count} views
-                    </span>
+                            {item.rating?.count} reviews
+                        </a>
+                    </Rating>
                 </div>
                 <div className="flex items-center justify-between">
                     <span className="text-3xl font-bold text-gray-900 ">${item.price}</span>
-                    <a
-                        href="#"
+                    <Link
+                        to={`/product/${item.id}/detail`}
                         className="transition-all text-white bg-slate-700 hover:bg-slate-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                     >
                         More detail
-                    </a>
+                    </Link>
                 </div>
             </div>
         </motion.div>
