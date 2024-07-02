9ddf8033efc84ee215b7747353a437cc2452e2be
Ktn.beb <krittanon.lak11@gmail.com>
2024-07-02 19:09:17 +0700

    edit footer

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -7,15 +7,17 @@ import Footer from "../../components/Footer/Footer";
 function About() {
     return (
         <>
-            <div className="h-[100vh] flex flex-col">
-                <NavBar />
-                <motion.hr
-                    className="border-slate-300"
-                    initial={{ width: 0, opacity: 0 }}
-                    animate={{ width: "100%", opacity: 1 }}
-                    transition={{ duration: 2 }}
-                />
-                <AboutUs />
+            <div className="min-h-[100vh] flex flex-col">
+                <div>
+                    <NavBar />
+                    <motion.hr
+                        className="border-slate-300"
+                        initial={{ width: 0, opacity: 0 }}
+                        animate={{ width: "100%", opacity: 1 }}
+                        transition={{ duration: 2 }}
+                    />
+                    <AboutUs />
+                </div>
                 <Footer />
             </div>
         </>
