571b4c3be4bc89bf4cc9a3ffd61b6862499a0b67
Ktn.beb <krittanon.lak11@gmail.com>
2024-07-02 15:43:48 +0700

    checkout form component

---
+++ src/pages/checkout/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import CheckoutForm from "../../components/Checkout/CheckoutForm";
+import NavBar from "../../components/Navbar/NavBar";
+import { motion } from "framer-motion";
+
+function Checkout() {
+    return (
+        <>
+            <NavBar />
+            <motion.hr
+                className="border-slate-300"
+                initial={{ width: 0, opacity: 0 }}
+                animate={{ width: "100%", opacity: 1 }}
+                transition={{ duration: 2 }}
+            />
+            <div>
+                <CheckoutForm />
+            </div>
+        </>
+    );
+}
+
+export default Checkout;
